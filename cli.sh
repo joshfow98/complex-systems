0ca4fd8792a63b4542c5684c194c81d145b75ca0
joshfow98 <32518194+joshfow98@users.noreply.github.com>
2020-04-30 23:29:12 +0100

    Made the CLI look nicer. Added a regex to the CLI so that only integers can be entered. Made the ratings review count look nicer (separator function). Changed regex for the ratings review count so that it does not allow the user to enter anything above 5.0 (previously 5.1 to 5.9 was allowed).

--- cli.sh
+++ cli.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
-declare -a options
+function separator() {
+    echo "+-----------------------------------------------------------+"
+}
+
+function user_options() {
+    separator
+    echo "The options are as follows: "
+    echo "1) Review Rating Count"
+    echo "2) Review Rating Count"
+    echo "3) Review Rating Count"
+    echo "4) Review Rating Count"
+    echo "5) Review Rating Count"
+    echo "6) Review Rating Count"
+    echo "7) Review Rating Count"
+    echo "8) Review Rating Count"
+    echo "9) Review Rating Count"
+    echo "10) Review Rating Count"
+    separator
+}
+
 options[1]="./ratings_review_count/ratings_review_count.sh"
 optionsLen=${#options[*]}
 
+separator
 echo "Welcome to Coursework 2 CLI!"
-echo "1) Review Rating Count"
+separator
+echo "This CLI allows you to choose any of the 10 options"
+echo "that will be presented. Choose your option by entering"
+echo "the number corresponding with that option."
+echo "To exit this program enter q."
+user_options
 
+#Input with only digits in will be accepted by this Regex
+numericRegex='^[0-9]+$'
 continueExecuting=true
 while [ $continueExecuting == true ]
 do
- read -p "Enter your option: " option
- if [ $option != 0 ] || [ $option > $optionsLen ]; then
-  sh ${options[$option]}
- elif [ $option == "q" ]; then
-  continueExecuting=false
- else 
-  echo $option " is not a valid option"
- fi
+    read -p "Enter your option or 'q' to exit: " option
+    if [[  $option =~ $numericRegex ]]; then
+        if [ $option -ne 0 ] | [ $option -le $optionsLen ]; then
+            echo "optionsLen is: " $optionsLen
+            sh ${options[$option]}
+            user_options
+        else 
+            echo $option "is not a valid option"
+        fi
+    elif [ $option == "q" ]; then
+        continueExecuting=false
+    else 
+        echo $option "is not a number"
+    fi
 done
--- ratings_review_count/ratings_review_count.sh
+++ ratings_review_count/ratings_review_count.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
-echo "This script will find the total number of reviews for the rating number you have entered."
-echo "For example, if you enter 5.0, the count for the number of reviews with a 5.0 rating will be shown"
+function separator() {
+    echo "+-----------------------------------------------------------+"
+}
 
-#Regex will allow doubles as well as integers
-regex='^[0-5]+(\.?[0-9]+)?$'
+separator
+echo "This script will find the total number of reviews for the"
+echo "rating number you have entered."
+echo "For example, if you enter 5.0, the count for the number of"
+echo "reviews with a 5.0 rating will be shown"
+separator
+
+#Regex will allow doubles as well as integers between 0.0 and 5.0
+regex='^[0-4]+(\.?[0-9]+)?|^5(\.0)?$'
 continueExecuting=true
 while [ $continueExecuting == true ]
 do
- read -p "Input your the rating you would like to count or 'q' to exit script: " rating
- if [[ $rating =~ $regex ]]; then
-  impala-shell --var=rating=$rating -f ratings_review_count.sql
- elif [ $rating == "q" ]; then
-  continueExecuting=false
- else
-  echo "Entered value is not valid"
- fi
+    read -p "Input your the rating or 'q' to exit: " rating
+    if [[ $rating =~ $regex ]]; then
+        separator
+        impala-shell --var=rating=$rating -f ./ratings_review_count/ratings_review_count.sql
+    elif [ $rating == "q" ]; then
+        continueExecuting=false
+    else
+        separator
+        echo "Entered value is not valid"
+    fi
+    separator
 done
