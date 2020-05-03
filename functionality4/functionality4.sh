dc74c1d704ef3d736fe8b03d5070da9c746baa36
jhudson85 <jack.hudson85@yahoo.co.uk>
2020-05-03 19:46:46 +0100

    Now only displaying the top 20 collocations
    Few edits to functionality4 to only display the 20 most common collocations.

--- functionality4/functionality4.sh
+++ functionality4/functionality4.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#Wrote by Jack
-function separator() {
+#Author: Jack
+
+function separator()
+{
     echo "+-----------------------------------------------------------+"
 }
 
-separator
-echo -e "This script takes a review score from 1 to 5 and \nreturns the three most common phrases associated with \nthose reviews."
-separator
-
+function getReviewRating()
+{
 #Any integer between 1-5 only.
 regex='^([1-5])$'
+
 inloop=true
 while [ $inloop == true ]
 do
@@ -20,11 +21,12 @@ do
       echo "This value is not between 1-5."
    fi
 done
+}
 
-echo "Finding the most common collocations for reviews with a rating of $rating (this may take a while)..."
-hive -silent -e "INSERT OVERWRITE DIRECTORY '/tmp/mapreduce_hive' SELECT reviewtext FROM coursework.reviews WHERE rating = $rating";
-
+function getOutputDirectory()
+{
 inloop=true
+
 while [ $inloop == true ]
 do
    read -p "Enter a new directory on HDFS to output results (e.g. /user/cloudera/my_hdfs_directory/collocations): " file_loc
@@ -36,8 +38,23 @@ do
       inloop=false
    fi
 done
+}
+
+#Script starts here
+separator
+echo -e "This script takes a review score from 1 to 5 and \nreturns the three most common phrases associated with \nthose reviews."
+separator
+
+#User will input the rating for the review and the directory on hdfs.
+getReviewRating
+getOutputDirectory
+echo -e "\nFinding the most common collocations for reviews with a rating of $rating (this may take a while)..."
+
+#Write the review text to /tmp/mapreduce_hive on the hdfs.
+hive -silent -e "INSERT OVERWRITE DIRECTORY '/tmp/mapreduce_hive' SELECT reviewtext FROM coursework.reviews WHERE rating = $rating";
 
 #Find the three most common collocations using mapreduce
 hadoop jar ./functionality4/CollocationCount.jar uk.ac.edgehill.WordCount /tmp/mapreduce_hive/000000_0 $file_loc
-#Sort the result of map reduce by descending values.
-hdfs dfs -cat $file_loc/part-r-00000 | sort -nk 4
+echo -e "\nCollocation | number of instances\n-------------------------------------"
+#Sort the result of map reduce by descending values and display the twenty most frequent collocations.
+hdfs dfs -cat $file_loc/part-r-00000 | sort -k 4 -n -r | head -n 20
