d5b03e9cadada907c6bc3ac232601eefd18c70b8
joshfow98 <32518194+joshfow98@users.noreply.github.com>
2020-05-14 08:57:56 +0100

    Update cli.sh
    Added comments to the CLI

--- cli.sh
+++ cli.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+#Used to separator different section of functionality on the screen for the user
 function separator() {
     echo "+-----------------------------------------------------------+"
 }
 
+#Used to display the users options
+#This is a function because it is used each time the user exits one of the options
 function user_options() { 
     separator
     echo "The options are as follows: "
@@ -24,6 +27,7 @@ function user_options() {
     separator
 }
 
+#All the starting scripts for each functionality are added to the 'options' array
 options[1]="./functionality1/functionality1.sh"
 options[2]="./functionality2/functionality2.sh"
 options[3]="./unavailible.sh"
@@ -41,6 +45,7 @@ options[14]="functionality14/functionality14.sh"
 options[15]="functionality15/functionality15.sh"
 optionsLen=${#options[*]}
 
+#Introduces the user to the program
 separator
 echo "Welcome to Coursework 2 CLI!"
 separator
@@ -50,22 +55,38 @@ echo "the number corresponding with that option."
 echo "To exit this program enter q."
 user_options
 
-#Input with only digits in will be accepted by this Regex
+#This Regex ensures that the user only enters numeric values for the options
 numericRegex='^[0-9]+$'
+#This variable will be used to terminate the program when the users enters 'q'
 continueExecuting=true
+#This loop will use the variable above to keep asking the user to enter
+#an option until the user enters 'q' to exit the program
 while [ $continueExecuting == true ]
 do
+    #Asks user what option they wish to choose
     read -p "Enter your option or 'q' to exit: " option
+    #This checks the users input against the previously declared regex
+    #If the input is invalid it will move to the 'elif' statement
     if [[  $option =~ $numericRegex ]]; then
+        #This checks that the inputted number is within the bounds of the 'options' array
+        #i.e. it is a valid option
         if [ $option -ne 0 ] | [ $option -le $optionsLen ]; then
+            #Should allow script to run ifit is not executable on the current system
+            chmod +x ${options[$option]}
+            #Executes the chosen functionality script
             sh ${options[$option]}
+            #Presents the user with the availables options once the previous functionality has ended 
             user_options
         else
+            #If the user has entered a non-valid option it outputs this below
             echo $option "is not a valid option"
         fi
+    #Checks if the user has entered the 'q' exit command
     elif [ $option == "q" ]; then
+        #Sets this variable to false so that the loop is terminated
         continueExecuting=false
     else
+        #This is outputed if the option the user enters is not a number
         echo $option "is not a number"
     fi
 done
