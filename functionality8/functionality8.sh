2bd5da762ee55046d735c2abe7e3e800cd138de8
joshfow98 <32518194+joshfow98@users.noreply.github.com>
2020-05-14 08:59:18 +0100

    Functionality 8
    Added comments to functionality 8 for both the shell script and python script

--- functionality8/functionality8.sh
+++ functionality8/functionality8.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#Used to separate the output on the screen to make it easier for the user to digest
 function separator() {
     echo "+-----------------------------------------------------------+"
 }
@@ -9,16 +10,29 @@ separator
 
 #Regex will allow doubles as well as integers between 0.0 and 5.0
 regex='\s'
+#Python script is called twice so put into variable to save repetition
+pythonScript='./functionality8/parse_response.py'
+#Variable used for continuing script until user enters 'q' to quit
 continueExecuting=true
+#Loop used to allow users to keep entering search terms until they quite using 'q'
 while [ $continueExecuting == true ]
 do
+    #Prompts user for input
     read -p "Input your word or 'q' to exit: " word
+    #Checks users input against the regex previously declared
     if [[ $word =~ $regex ]]; then
+        #Tells user their input is invalid if it is invalidated by the regex
         echo $word " not valid, only single words and no spaces allowed"
-    elif [ $word != "q" ]; then 
+    #Checks if the user has entered the 'q' exit command
+    elif [ $word != "q" ]; then
+        #Makes a HTTP request to elasticsearch for the users inputted value 
         response=$(curl -s -X GET "http://localhost:9200/hivereviews/_search?q=$word&filter_path=hits.hits._source&_source=gplususerid,gplusplaceid")
-        python ./functionality8/parse_response.py $response $word
+        #Ensures that the python script is executable
+        chmod +x $pythonScript
+        #Executes the python script to parse the cURL response and output it to the user
+        python $pythonScript $response $word
     else
+        #Stops the loop from continuing to execute
         continueExecuting=false
     fi
 done
--- functionality8/parse_response.py
+++ functionality8/parse_response.py
@@ -1,12 +1,19 @@
+#For parsing JSON data from cURL
 import json
 #for receiving arguments
 import sys
 
+#Loads the inputted cURL  JSON response as a JSON object
 parsedResponse = json.loads(sys.argv[1])
+#Removes uneeded data from the JSON response
 reviewIds = parsedResponse["hits"]["hits"]
-outputs = []
+#Used Python output from previous script output
 print("+--------------------------------------------------------------+")
+#Prints the users inputted value back to the mbefore showing the response
 print(sys.argv[2])
+#Loops through all the different records recieved from cURL
 for item in reviewIds:
+    #Creates an output string that contains the records gplusplaceid and gplusuerid
     output = item["_source"]["gplusplaceid"] + "_" + item["_source"]["gplususerid"]
+    #Outputs the gplusplaceid and gplususerid to the user
     print(output)
