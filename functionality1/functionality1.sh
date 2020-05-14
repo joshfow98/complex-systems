1c61e0b5b709802aecfa7ae25af5683d54fdf7ec
joshfow98 <32518194+joshfow98@users.noreply.github.com>
2020-05-14 08:58:44 +0100

    Functionality 1
    Added comments to functionality 1, both the script and SQL

--- functionality1/functionality1.sh
+++ functionality1/functionality1.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+#Written by Josh
+#Used to separate the different outputs to make it easier for the user to digest
 function separator() {
     echo "+-----------------------------------------------------------+"
 }
 
+#Outputs adescription of the functionality to the user
 separator
 echo "This script will find the total number of reviews for the"
 echo "rating number you have entered."
@@ -12,18 +15,29 @@ separator
 
 #Regex will allow doubles as well as integers between 0.0 and 5.0
 regex='^[0-4]+(\.?[0-9]+)?|^5(\.0)?$'
+#Variable used to keep the functionality working until the user enters the 'q' exit command
 continueExecuting=true
+#This loop keeps executing until the user enters the 'q' exit command
 while [ $continueExecuting == true ]
 do
+    #Prompts the user for input
     read -p "Input your the rating or 'q' to exit: " rating
+    #Checks the user input against the regex to see if it matches
     if [[ $rating =~ $regex ]]; then
+        #separates the user previous output from the the output about to occur
         separator
-        impala-shell --var=rating=$rating -f ./functionality1/functionality1.sql
+        #Runs the functionality 1 SQL command using impala, passing the input as a parameter
+        impala-shell --quiet --var=rating=$rating -f ./functionality1/functionality1.sql
+    #Checks if the user entered the 'q' exit command
     elif [ $rating == "q" ]; then
+        #Changed to terminate the loop to exit the functionality
         continueExecuting=false
     else
+        #Separates the previous output from the next output
         separator
+        #Prompts the user that their input was invalid
         echo "Entered value is not valid"
     fi
+    #Separates the next output from the previous output
     separator
 done
--- functionality1/functionality1.sql
+++ functionality1/functionality1.sql
@@ -1,1 +1,6 @@
+/*
+    This script takes input via a parameter and outputs the count of all 
+    reviews that have the same rating as the inputted value
+    *Written by Josh*
+*/
 SELECT COUNT(*) FROM coursework.reviews AS r WHERE r.rating = ${var:rating};
